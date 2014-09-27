790254ebf2ff43424b83ccdb9bd1bd81ea8e83ba
Bartosz J <thug1337@gmail.com>
2014-09-27 18:13:03 +0200

    Fix issue #25: - Omit SCRIPT_VALUE_TYPE_NULL entries with no key name

--- script_bin.c
+++ script_bin.c
@@ -289,8 +289,11 @@ static int decompile_section(void *bin, size_t bin_size,
 						   v))
 				goto malloc_error;
 			}; break;
-		case SCRIPT_VALUE_TYPE_NULL:
-			if (!script_null_entry_new(s, entry->name))
+		case SCRIPT_VALUE_TYPE_NULL:	// decompiling null entry makes no sense if it has no name
+			if (!*entry->name) {
+				pr_err("%s: empty entry in section: %s\n", filename, section->name);
+			}
+			else if (!script_null_entry_new(s, entry->name))
 				goto malloc_error;
 			break;
 		default:
